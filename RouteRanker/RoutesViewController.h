602a0d728b283d1d6ce8f08ccd6cb863387d5e01
Nick Gilligan <ngilligan624@yahoo.com>
2012-04-24 22:13:11 -0400

    asdf

---
+++ RouteRanker/RoutesViewController.h
@@ -0,0 +1,15 @@
+//
+//  RoutesViewController.h
+//  RouteRanker
+//
+//  Created by Nick Gilligan on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoutesViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *routes;
+
+@end
